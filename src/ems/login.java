571495b9601102722fdc526a407c678e77cb85d3
AymaneELRHYATI <100347053+AymaneELRHYATI@users.noreply.github.com>
2024-04-26 00:14:00 +0100

    Add files via upload

--- src/ems/login.java
+++ src/ems/login.java
@@ -98,6 +98,11 @@ public class login extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(51, 153, 255));
         jLabel4.setText("Username/Password?");
+        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel4MouseClicked(evt);
+            }
+        });
 
         btnlogin.setBackground(new java.awt.Color(51, 153, 255));
         btnlogin.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
@@ -217,10 +222,15 @@ public class login extends javax.swing.JFrame {
                 rs = ps.executeQuery();
                 
                 if(rs.next()){
+                    String name = rs.getString(3); 
+                    String des = rs.getString(5);
+                    
                     JOptionPane.showMessageDialog(null,"Log in succ ");
-                    MainMenu m = new MainMenu();
+                    MainForm m = new MainForm(name,des); 
                     m.setVisible(true);
                     this.dispose();
+                    
+                    
                 }else{
                     
                     JOptionPane.showMessageDialog(null,"Log in Failed ");
@@ -245,6 +255,20 @@ public class login extends javax.swing.JFrame {
             Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jLabel5MouseClicked
+
+    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
+        try {
+            // TODO add your handling code here:
+            forgotForm f = new forgotForm();
+            f.setVisible(true); 
+            this.dispose();
+        } catch (SQLException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+    }//GEN-LAST:event_jLabel4MouseClicked
 
     /**
      * @param args the command line arguments
